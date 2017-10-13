ca022668f90d0481e58418e55d61c0ad340f10fc
Kathy Chiu <chiuy5@uw.edu>
2017-10-13 11:39:22 -0700

    added code

--- labs/javascript/lab.js
+++ labs/javascript/lab.js
@@ -1,2 +1,8 @@
 // Put your JavaScript lab code here!
+var str = "I like JavaScript more than I like to party";
+print(str.splice(28, str.length - 1));
 
+var str2 = "I love Washington State University";
+str2 = str2.replace("Washington State Univeristy", "The University of Washington");
+
+var str3 = str.concat(str2);
